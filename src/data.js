efdf9802ef44583c1a830a314ac04dcc41a55de4
Karolin Valaszkai <karolinv@kth.se>
2018-08-20 17:12:18 +0200

    k

--- src/data.js
+++ src/data.js
@@ -1,4 +1,68 @@
-export default [
+import React, { Component } from 'react';
+import { bake_cookie, read_cookie } from 'sfcookies';
+
+// se youtube-vid
+/* state = read_cookie('items');
+bake_cookie('items', items); */
+
+
+class data extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://dog.ceo/api/breeds/image/random/10")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result.items
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+
+  }
+  render() {
+    const { error, isLoaded, items } = this.state;
+    console.table(items)
+
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <ul>
+          {items.map(item => (
+            <li>
+              {items.message}
+            </li>
+          ))}
+        </ul>
+      );
+    }
+  }
+}
+export default data;
+
+
+/* export default [
   {
     answers: ['Sweden', 'USA', 'France', 'Italy'],
     correct: 3,
@@ -50,3 +114,4 @@ export default [
     coordinates: { lat: 29.979175, lng: 31.134358 }
   }
 ];
+ */
